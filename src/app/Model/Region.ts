29c26ac529c1ebafe44a281056e23f1abf7a2842
aneesh babu <bob@example.com>
2020-04-27 16:23:46 +0530

    DEMI COMMIT

---
+++ src/app/Model/Region.ts
@@ -0,0 +1,9 @@
+export class Region
+{
+    loc:string;
+    confirmedCasesIndian:number;
+    discharged:number;
+    deaths:number;
+    confirmedCasesForeign:number;
+    totalConfirmed:number;
+}
